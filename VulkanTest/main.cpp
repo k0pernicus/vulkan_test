338a69aa970a1bd7460d41a09461dcc5c4c46dc6
k0pernicus <dev@0xc0ff33.me>
2022-06-12 08:19:29 +0200

    Initial Commit

---
+++ VulkanTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTest
+//
+//  Created by Antonin on 12/06/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
